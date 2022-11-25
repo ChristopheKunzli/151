123989a5f27099cce74ce2284291a0b66411836e
Christophe.KUNZLI <Christophe.KUNZLI@cpnv.ch>
2022-11-25 10:06:26 +0100

    update

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const exphbs = require('express-handlebars')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
-const crypto = require('crypto')
 const app = express()
 
-//const pool = require('./db/dbconfig')
+const pool = require('./db/dbconfig')
 
+const crypto = require('crypto')
 const authTokens = {}
 
 const getHashedPassword = (password) => {
@@ -30,14 +30,25 @@ app.use((req, res, next) => {
     next()
 })
 
+//========================================================================//
+// Static files
+//TODO move directories away from root dir
+//========================================================================//
+app.use('/images', express.static('images'))
+
+app.use('/css', express.static('css'))
+
+app.use('/js', express.static('js'))
+
+app.use('/vendor', express.static('vendor'))
+//========================================================================//
+
 app.engine('hbs', exphbs.engine({
     extname: '.hbs'
 }))
 
 app.set('view engine', 'hbs')
 
-//Database
-const pool = require("./db/dbconfig")
 
 //========================================================================//
 // Routes
@@ -56,6 +67,56 @@ app.get('/home', (req, res) => {
         res.render('home', {row: r})
     })
 })
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.post('/auth', (req, res) => {
+    const mail = req.body.email
+    const pass = req.body.password
+
+    const cmd = "INSERT INTO users ('email', 'password') VALUES ('test@cpnv.ch', 'passassass');"
+
+    if (mail && pass) {
+        pool.query(cmd, (error, res, fields) =>{
+            if(error) throw error
+            res.redirect('/about')
+        })
+    } else {
+        res.redirect('/contact')
+    }
+})
+
+/**
+ app.post('/auth', (req, res) => {
+// Capture the input fields
+    let user_email = req.body.email;
+    let password = req.body.userPswd;
+    // Ensure the input fields exists and are not empty
+    if (user_email && password) {
+        // Execute SQL query that'll select the account from the database based on the specified username and password
+        pool.query('SELECT * FROM users WHERE email = ? AND password = ?', [user_email, password], (error, results, fields) => {
+            // If there is an issue with the query, output the error
+            if (error) throw error;
+            // If the account exists
+            if (results.length > 0) {
+                // Authenticate the user
+                req.session.loggedin = true;
+                req.session.username = user_email;
+                // Redirect to home page
+                res.redirect('/home');
+            } else {
+                res.send('Incorrect Username and/or Password!');
+            }
+            res.end();
+        });
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+})
+ */
 
 app.get('/about', (req, res) => {
     res.render('about')
