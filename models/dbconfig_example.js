5294441bbcf14ed15953bd7ccbf02a2fd9e467cb
KUNZLI Christophe <Christophe.KUNZLI@cpnv.ch>
2022-12-02 11:45:59 +0100

    remove files

--- models/dbconfig_example.js
+++ models/dbconfig_example.js
@@ -2,9 +2,9 @@ const mariadb = require('mariadb')
 
 const pool = mariadb.createPool({
     host: 'localhost',
-    user: 'admin_test_151',
-    password: 'Pa$$w0rd',
-    database: 'test'
+    user: 'your_user',
+    password: 'your_password',
+    database: 'your_db'
 })
 
 module.exports = pool
