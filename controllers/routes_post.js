0617fdb654e5ddfc410cd6f50f1bcdbf98fc316e
ChristopheKunzli <Christophe.kunzli@cpnv.ch>
2022-12-18 12:51:53 +0100

    add tasks to implement next

--- controllers/routes_post.js
+++ controllers/routes_post.js
@@ -1,5 +1,11 @@
 module.exports = (app, pool) => {
     app.post('/add', (req, res) => {
+        //TODO check if user is logged in
 
+        //TODO fetch all data of the form
+
+        //TODO build the INSERT INTO sql query
+
+        //TODO execute the query into the db and redirect the user to management page
     })
 }
